3bd3aee28e87c5ad3c7efa73fb23d668cbde6ad3
Selva1907 <153350984+Selva1907@users.noreply.github.com>
2023-12-15 09:13:35 +0530

    Add files via upload
    Login and Signup Page for Project

---
+++ ems/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import LoginPage from './Componentes/LoginPage';
+//import LoginPage from './Componentes/LoginPage';
+//import SignupPage from './Componentes/SignupPage';
+
+function App() {
+  return (
+    <div className="App">
+      
+     <LoginPage />
+    </div>
+  );
+}
+
+export default App;
